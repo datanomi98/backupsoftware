6b772e086385e546766b05e093580d78d3d844b9
datanomi98 <datanomi@protonmail.com>
2018-01-29 08:43:16 +0200

    Update backupserver.cs
    made some improvements for getting the filename.

--- backupsoftware/backupserver.cs
+++ backupsoftware/backupserver.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,8 +15,8 @@ namespace backupserver
         public static string nameOfTheFile;
         static void Main(string[] args)
         {
-            start:
-            getFilename();
+           
+            
             Socket soc = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
             IPAddress ipReceive = IPAddress.Parse("your ip here");
             var listener = new TcpListener(ipReceive, 11000);
@@ -28,43 +28,37 @@ namespace backupserver
                 
                 using (var client = listener.AcceptTcpClient())
                 using (var stream = client.GetStream())
-                using (var output = File.Create(nameOfTheFile))
                 {
 
-                    var buffer = new byte[1024];
-                    int bytesRead;
+                    var fileNamebuffer = new byte[512];
+                    int filenamebytesRead;
                     Console.WriteLine("Client connected. Starting to receive the file");
-                    while ((bytesRead = stream.Read(buffer, 0, buffer.Length)) > 0)
+					 while ((filenamebytesRead = stream.Read(fileNamebuffer, 0, fileNamebuffer.Length)) <= 512)
+                    {
+						nameOfTheFile = bytesRead;
+					}
+					
+					if(nameOfTheFile != ""){
+						
+					using (var output = File.Create(nameOfTheFile)){
+					 var buffer = new byte[1024];
+                    int bytesRead;
+				   while ((bytesRead = stream.Read(buffer, 0, buffer.Length)) > 0)
                     {
 
                         output.Write(buffer, 0, bytesRead);
 
                     }
                     listener.Stop();
-                    goto start;
+                    }
                 }
+			}
 
                
             }
            
         }
-        static void getFilename()
-        {
-
-            var data = new byte[1024];
-            IPEndPoint ServerEndPoint = new IPEndPoint(
-               IPAddress.Parse("your ip here"), 9050);
-            Socket WinSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
-            WinSocket.Bind(ServerEndPoint);
-            Console.WriteLine("Waiting for client");
-            IPEndPoint sender = new IPEndPoint(
-               IPAddress.Parse("your ip here"), 0);
-            EndPoint Remote = (EndPoint)(sender);
-            int recv = WinSocket.ReceiveFrom(data, ref Remote);
-            string returnFilename = Encoding.ASCII.GetString(data, 0, recv);
-            nameOfTheFile = returnFilename;
-            WinSocket.Close();
-        }
+       
        
     }
 }
